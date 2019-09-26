cfa6de73c40b97b695cc3e579826b0bdc3012835
mccdqw <54416592+mccdqw@users.noreply.github.com>
2019-09-25 20:57:48 -0500

    Create mainfile.py

---
+++ Assignment-Five/tests/mainfile.py
@@ -0,0 +1,26 @@
+def getUsername():
+    username = 'mccdqw'
+    return username
+
+def getPassword():
+    password = 'password'
+    return password
+
+def comment():
+    comment = 'the assignment is late'
+    return comment
+
+def searchStudent():
+    studentList = ['bob', 'ann', 'steve']
+    return studentList
+
+def lateSubmission(submissionDate):
+    submissionDate = 1
+    if submissionDate > 1:
+        return False
+    else:
+        return True
+
+def selectAssignment():
+    assignmentList = ['assgn1', 'assgn2', 'assgn3']
+    return assignmentList
